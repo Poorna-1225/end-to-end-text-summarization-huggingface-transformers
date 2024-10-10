4f4b47446a20f1a26211cdfa0a33184677ea612d
Poorna-1225 <nagasaikumarjampani1998@gmail.com>
2024-10-09 23:06:06 -0400

    data transformation stage is added to the training pipeline

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,7 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import *
-from textSummarizer.entity import DataIngestionConfig, DataValidationConfig
+from textSummarizer.entity import (DataIngestionConfig, DataValidationConfig,
+                                   DataTransformationConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -39,3 +40,16 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    def get_data_transform_config(self)-> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir = config.root_dir,
+            tokenizer_name= config.tokenizer_name,
+            data_path= config.data_path,
+        )
+        return data_transformation_config
+                 
